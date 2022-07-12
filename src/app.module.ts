21932d499399418a61a1a520dbcd3ccfe876fb47
DaveT123 <sinchin9001@hotmail.com>
2022-07-12 20:14:57 +0800

    fix error with todos module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
-import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [UsersModule, TodosModule],
